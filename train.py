2836711f5f77d69bcf30bc4023f469ab933078a2
Arkar Aung <arica.aung@gmail.com>
2020-02-19 20:54:55 +0630

    Add step by step comments for training procedure

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from models.simpleConv import SimpleConv
 import torch
-import torch.nn
+import torch.nn as nn
+import torch.optim as optim
 
 if __name__ == "__main__":
     net = SimpleConv()
@@ -11,16 +12,45 @@ if __name__ == "__main__":
     # Forward pass
     output = net(input_mat)    
     
-    # Clear parameter buffer
-    net.zero_grad()
+    # Clear gradients otherwise the gradients will be accumulated
+    # net.zero_grad()
 
-    # Backpropagate
-    output.backward(torch.randn(1,10))
+    # Backpropagate with respect to output - this is actually just for the sake of demo 
+    # You have you backgpropagate with respect to loss
+    # output.backward(torch.randn(1,10))
 
+    # Random target
     target = torch.randn(10)
+
+    # Modify the dimension of target so that it matches with neural network output
     target = target.view(1, -1)
+
+    # Define the type of loss function
     criterion = nn.MSELoss()
 
+    # Calculate the loss between output and target
     loss = criterion(output, target)
-    print(loss)
+
+    # Zero the gradient buffers of all parameters
+    net.zero_grad()
+
+    print('conv1.bias.grad before backprop')
+    print(net.conv1.bias.grad)
+
+    loss.backward()
+
+    print('conv1.bias.grad after backprop')
+    print(net.conv1.bias.grad)
+
+    # create your optimizer - Stochastic Gradient Descent
+    optimizer = optim.SGD(net.parameters(), lr=0.01)
+
+    optimizer.zero_grad() # zero the gradient buffers -- because of the above line this is same as saying net.zero_grad()
+                          # another difference is that if you have different optimizers for different parts of the model, 
+                          # model.zero_grad() would clear all parameters of the model, while the optimizer.zero_grad() call will just 
+                          # clean the gradients of the parameters that were passed to it.
+    output = net(input_mat)
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step() # Does gradient update, which updates the weights
 
