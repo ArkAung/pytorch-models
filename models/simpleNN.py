32521b3eeb5818e22f10a9e797aca654d06b7bdf
Arkar Aung <arica.aung@gmail.com>
2020-02-19 13:32:57 +0630

    Add simple 2 layer fc network

---
+++ models/simpleNN.py
@@ -0,0 +1,17 @@
+"""
+Simple 2 layer Fully Connected Neural Network
+
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleNN(nn.Module):
+    def __init__(self):
+        self.fc1 = nn.Linear(10,10)
+        self.fc2 = nn.Linear(20,10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return x
